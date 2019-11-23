067dad11a9049c3a2db3d3b2e1c41504c95281b1
jeanropke <jeanropke@ymail.com>
2019-11-23 00:01:36 -0300

    Added new heatmaps

--- assets/js/heatmap.js
+++ assets/js/heatmap.js
@@ -5,30 +5,30 @@
 var Heatmap = {};
 
 Heatmap.load = function() {
-    $.getJSON(`data/heatmap.json?nocache=${nocache}`)
-        .done(function(data)
-        {
-            Heatmap.data = data;
-            Heatmap.init();
-        });
+  $.getJSON(`data/heatmap.json?nocache=${nocache}`)
+    .done(function(data) {
+      Heatmap.data = data;
+      Heatmap.init();
+    });
 };
 
-Heatmap.init = function () {
+Heatmap.init = function() {
 
-    $.each(Heatmap.data, function(heatmapKey, heatmapValue)
-    {
-        $.each(heatmapValue, function(key, value)
-        {
-            $(`.${heatmapKey}`).append(`<div class="menu-row">
+  $.each(Heatmap.data, function(heatmapKey, heatmapValue) {
+    $.each(heatmapValue, function(key, value) {
+      if (value.data.length == 0)
+        return;
+        
+      $(`.${heatmapKey}`).append(`<div class="menu-row">
              <div class="menu-option animal-clickable" data-type="${key}">
              <img class="icon" src="https://s.rsg.sc/sc/images/games/RDR2/compendium/CMPNDM_ANIMALS/${value.image}" />
-             <span data-text="menu.animal.${key}" class="disabled">${Language.get('menu.animal.'+key)}</span>
+             <span data-text="menu.${heatmapKey}.${key}" class="disabled">${Language.get(`menu.${heatmapKey}.${key}`)}</span>
              </div>
              </div>`);
-        });
     });
+  });
 
-    //Reorder animals menu alphabetically
-    Menu.reorderMenu('.animals');
-    Menu.reorderMenu('.birds');
+  //Reorder animals menu alphabetically
+  Menu.reorderMenu('.animals');
+  Menu.reorderMenu('.birds');
 };
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,62 +2,50 @@
  * Created by Jean on 2019-10-09.
  */
 
-var Menu = {};
-
-Menu.refreshMenu = function (isFirstLoad = false)
-{
-    if(isFirstLoad) return;
+var Menu = {
+  refreshMenu: function(isFirstLoad = false) {
+    if (isFirstLoad) return;
     var countPlantsType = {};
     var precisePlants = 0;
     var notPrecisePlants = 0;
     var totalMarkersPlants = 0;
 
-    markers.forEach(function(item)
-    {
-        if(item.sub_data != null)
-        {
-            if(item.count == null) {
-                countPlantsType[item.sub_data] = (countPlantsType[item.sub_data] || 0) + 1;
-                notPrecisePlants++;
-            }
-            else {
-                countPlantsType[item.sub_data] = (countPlantsType[item.sub_data] || 0) + parseInt(item.count);
-                precisePlants += parseInt(item.count);
-            }
-            totalMarkersPlants++;
+    markers.forEach(function(item) {
+      if (item.sub_data != null) {
+        if (item.count == null) {
+          countPlantsType[item.sub_data] = (countPlantsType[item.sub_data] || 0) + 1;
+          notPrecisePlants++;
+        } else {
+          countPlantsType[item.sub_data] = (countPlantsType[item.sub_data] || 0) + parseInt(item.count);
+          precisePlants += parseInt(item.count);
         }
+        totalMarkersPlants++;
+      }
     });
 
     console.info(`${precisePlants + notPrecisePlants} plants added (${totalMarkersPlants} markers)`);
     console.info(`${precisePlants} precise plants (${totalMarkersPlants - notPrecisePlants} markers)`);
     console.info(`${notPrecisePlants} not precise plants (${notPrecisePlants} markers)`);
 
-    $.each(categories, function (key, value)
-    {
-        markers.filter(function(item)
-        {
+    $.each(categories, function(key, value) {
+      markers.filter(function(item) {
+        if (!categories.includes(item.icon)) {
+          console.log(`%c O seu animal, coloca a categoria '${item.icon}' em 'categories' ou o menu não vai funcionar`, 'background: #222; color: #bada55; font-size:20px');
+        }
 
-            if(!categories.includes(item.icon)){
-                console.log(`%c O seu animal, coloca a categoria '${item.icon}' em 'categories' ou o menu não vai funcionar`, 'background: #222; color: #bada55; font-size:20px');
-            }
+        if (item.icon == value) {
+          if (item.sub_data == null)
+            return;
 
-            if(item.icon == value)
-            {
-                if(item.sub_data == null)
-                    return;
+          if ($(`.menu-hidden[data-type='plants']`).children(`p.collectible[data-type='${item.sub_data}']`).length > 0)
+            return;
 
-                if($(`.menu-hidden[data-type='plants']`).children(`p.collectible[data-type='${item.sub_data}']`).length > 0)
-                    return;
+          if (!subCategories.includes(item.sub_data))
+            console.log(`%c O seu animal, coloca a categoria '${item.sub_data}' em 'subCategories' ou o menu não vai funcionar`, 'background: #222; color: #bada55; font-size:20px');
 
-
-                if(!subCategories.includes(item.sub_data)){
-                    console.log(`%c O seu animal, coloca a categoria '${item.sub_data}' em 'subCategories' ou o menu não vai funcionar`, 'background: #222; color: #bada55; font-size:20px');
-                }
-
-
-                $(`.menu-hidden[data-type=${value}]`).append(`<p class="collectible" data-type="${item.sub_data}"><span data-text="menu.plant.${item.sub_data}">${Language.get('menu.plant.'+item.sub_data)}</span> <span class="item-count">${countPlantsType[item.sub_data]}</span></p>`);
-            }
-        });
+          $(`.menu-hidden[data-type=${value}]`).append(`<p class="collectible" data-type="${item.sub_data}"><span data-text="menu.plant.${item.sub_data}">${Language.get('menu.plant.'+item.sub_data)}</span> <span class="item-count">${countPlantsType[item.sub_data]}</span></p>`);
+        }
+      });
     });
 
     //Reorder plants menu alphabetically
@@ -66,32 +54,29 @@ Menu.refreshMenu = function (isFirstLoad = false)
     //Reorder animals menu alphabetically
     Menu.reorderMenu('.animals');
     Menu.reorderMenu('.birds');
+  },
 
-};
 
-Menu.reorderMenu = function (menu) {
+  reorderMenu: function(menu) {
     $(menu).children().sort(function(a, b) {
-        return a.textContent.localeCompare(b.textContent);
+      return a.textContent.localeCompare(b.textContent);
     }).appendTo(menu);
-};
+  },
 
-Menu.showAll = function() {
-    $.each (categoryButtons, function (key, value) {
-        $(value).children('span').removeClass("disabled")
+  showAll: function() {
+    $.each(categoryButtons, function(key, value) {
+      $(value).children('span').removeClass("disabled")
     });
     enabledTypes = categories;
     MapBase.addMarkers();
-};
+  },
 
-Menu.hideAll = function()
-{
-    $.each (categoryButtons, function (key, value) {
-        if($(value).data('type') != 'plants')
+  hideAll: function() {
+    $.each(categoryButtons, function(key, value) {
+      if ($(value).data('type') != 'plants')
         $(value).children('span').addClass("disabled")
     });
-
     enabledTypes = ['plants'];
     MapBase.addMarkers();
+  }
 };
-
-
