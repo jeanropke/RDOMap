8274557649eda454a5d763603f803106439433ef
jeanropke <jeanropke@ymail.com>
2020-01-15 10:16:08 -0300

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,32 +3,21 @@
  */
 
 var Menu = {
-  refreshMenu: function(isFirstLoad = false) {
+  refreshMenu: function (isFirstLoad = false) {
     if (isFirstLoad) return;
     var countPlantsType = {};
-    var precisePlants = 0;
-    var notPrecisePlants = 0;
-    var totalMarkersPlants = 0;
-
-    markers.forEach(function(item) {
+    markers.forEach(function (item) {
       if (item.sub_data != null) {
         if (item.count == null) {
           countPlantsType[item.sub_data] = (countPlantsType[item.sub_data] || 0) + 1;
-          notPrecisePlants++;
         } else {
           countPlantsType[item.sub_data] = (countPlantsType[item.sub_data] || 0) + parseInt(item.count);
-          precisePlants += parseInt(item.count);
         }
-        totalMarkersPlants++;
       }
     });
 
-    console.info(`${precisePlants + notPrecisePlants} plants added (${totalMarkersPlants} markers)`);
-    console.info(`${precisePlants} precise plants (${totalMarkersPlants - notPrecisePlants} markers)`);
-    console.info(`${notPrecisePlants} not precise plants (${notPrecisePlants} markers)`);
-
-    $.each(categories, function(key, value) {
-      markers.filter(function(item) {
+    $.each(categories, function (key, value) {
+      markers.filter(function (item) {
         if (!categories.includes(item.icon)) {
           console.log(`%c O seu animal, coloca a categoria '${item.icon}' em 'categories' ou o menu não vai funcionar`, 'background: #222; color: #bada55; font-size:20px');
         }
@@ -43,7 +32,7 @@ var Menu = {
           if (!subCategories.includes(item.sub_data))
             console.log(`%c O seu animal, coloca a categoria '${item.sub_data}' em 'subCategories' ou o menu não vai funcionar`, 'background: #222; color: #bada55; font-size:20px');
 
-          $(`.menu-hidden[data-type=${value}]`).append(`<p class="collectible" data-type="${item.sub_data}"><span data-text="menu.plant.${item.sub_data}">${Language.get('menu.plant.'+item.sub_data)}</span> <span class="item-count">${countPlantsType[item.sub_data]}</span></p>`);
+          $(`.menu-hidden[data-type=${value}]`).append(`<p class="collectible" data-type="${item.sub_data}"><span data-text="menu.plant.${item.sub_data}">${Language.get('menu.plant.' + item.sub_data)}</span> <span class="item-count">${countPlantsType[item.sub_data]}</span></p>`);
         }
       });
     });
@@ -58,22 +47,22 @@ var Menu = {
   },
 
 
-  reorderMenu: function(menu) {
-    $(menu).children().sort(function(a, b) {
+  reorderMenu: function (menu) {
+    $(menu).children().sort(function (a, b) {
       return a.textContent.localeCompare(b.textContent);
     }).appendTo(menu);
   },
 
-  showAll: function() {
-    $.each(categoryButtons, function(key, value) {
+  showAll: function () {
+    $.each(categoryButtons, function (key, value) {
       $(value).children('span').removeClass("disabled")
     });
     enabledTypes = categories;
     MapBase.addMarkers();
   },
 
-  hideAll: function() {
-    $.each(categoryButtons, function(key, value) {
+  hideAll: function () {
+    $.each(categoryButtons, function (key, value) {
       if ($(value).data('type') != 'plants')
         $(value).children('span').addClass("disabled")
     });
