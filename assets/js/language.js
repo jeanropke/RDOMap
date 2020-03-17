6ae98aeef4205232eaab104a1ee50d3dc9e090a0
Senexis <timoviveen@gmail.com>
2020-03-17 20:57:57 +0100

    Update langs and add Swedish language.

--- assets/js/language.js
+++ assets/js/language.js
@@ -3,7 +3,7 @@
  */
 
 var Language = {
-    availableLanguages: ['ar-ar', 'de-de', 'en-us', 'es-es', 'fr-fr', 'hu-hu', 'it-it', 'ko', 'pt-br', 'pl', 'ru', 'th-th', 'zh-hans', 'zh-hant'],
+    availableLanguages: ['ar-ar', 'de-de', 'en-us', 'es-es', 'fr-fr', 'hu-hu', 'it-it', 'ko', 'pt-br', 'pl', 'ru', 'sv-se', 'th-th', 'zh-hans', 'zh-hant'],
 
     get: function (value) {
         if (Settings.language == null)
