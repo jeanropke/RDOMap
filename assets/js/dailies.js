e7afb8abdcb81e4452f85de0b69fb2e777aafbb4
Michal__d <53737762+MichalD96@users.noreply.github.com>
2020-09-02 01:05:35 +0200

    Update dailies.js

--- assets/js/dailies.js
+++ assets/js/dailies.js
@@ -32,11 +32,8 @@ class Dailies {
   appendToMenu() {
     // TODO: This format needs to be translatable. Russian uses "<challenge>: 0/<goal>",
     // otherwise it doesn't make sense grammatically. - Bob
-    const structure = Language.get('menu.daily_challenge_structure').match(/\{(.+?)\}\s?\{(.+?)\}/);
-    $('.one-daily-container').css({
-      'grid-template-areas': `\"${structure[1]} ${structure[2]}\"`
-      
-      });
+    const structure = Language.get('menu.daily_challenge_structure').match(/^\{(.+?)\}\s?\{(.+?)\}$/);
+    $('.one-daily-container').css({ 'grid-template-areas': `\"${structure[1]} ${structure[2]}\"` });
 
     $(`.dailies > .${this.role}`).append($(`
       <div class="one-daily-container">
