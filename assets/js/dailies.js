d425d2757d9423077d7a9686b7ff1f6b64a049d8
Michal__d <53737762+MichalD96@users.noreply.github.com>
2020-09-03 06:16:44 +0200

    Return promise from dailies init

--- assets/js/dailies.js
+++ assets/js/dailies.js
@@ -15,7 +15,7 @@ class Dailies {
     const websiteData = Loader.promises['daily'].consumeJson(data => this.dailies = data.dailies);
     const allDailies = Loader.promises['possible_dailies'].consumeJson(data => this.jsonData = data);
 
-    Promise.all([websiteData, allDailies])
+    return Promise.all([websiteData, allDailies])
       .then(() => {
         console.info(`%c[Dailies] Loaded in ${Date.now() - start}ms!`, 'color: #bada55; background: #242424');
 
