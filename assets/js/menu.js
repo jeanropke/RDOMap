aec6e2a1fd563604f38bcd9edd7c31d5d0eabc1c
jeanropke <jeanropke@ymail.com>
2020-09-09 10:24:05 -0300

    Show/hide all buttons

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -23,44 +23,66 @@ class Menu {
         $helpParagraph.html(Language.get(`help.${helpTransId}`));
       });
 
+    $('.menu-hide-all').on('click', function () {
+      Shop.locations.forEach(shop => { if (shop.onMap) shop.onMap = !shop.onMap });
+      GunForHire.locations.forEach(_gfh => { if (_gfh.onMap) _gfh.onMap = !_gfh.onMap });
+      Encounter.locations.forEach(_encounter => { if (_encounter.onMap) _encounter.onMap = !_encounter.onMap });
+      PlantsCollection.locations.forEach(_plants => { if (_plants.onMap) _plants.onMap = !_plants.onMap });
+      Camp.locations.forEach(camp => { if (camp.onMap) camp.onMap = !camp.onMap });
+      Location.locations.forEach(loc => { if (loc.onMap) loc.onMap = !loc.onMap });
+      MadamNazar.onMap = false;
+      Pins.onMap = false;
+    });
+    $('.menu-show-all').on('click', function () {
+      Shop.locations.forEach(shop => { if (!shop.onMap) shop.onMap = !shop.onMap });
+      GunForHire.locations.forEach(_gfh => { if (!_gfh.onMap) _gfh.onMap = !_gfh.onMap });
+      Encounter.locations.forEach(_encounter => { if (!_encounter.onMap) _encounter.onMap = !_encounter.onMap });
+      PlantsCollection.locations.forEach(_plants => { if (!_plants.onMap) _plants.onMap = !_plants.onMap });
+      setTimeout(() => PlantsCollection.layer.redraw(), 40);
+      Camp.locations.forEach(camp => { if (!camp.onMap) camp.onMap = !camp.onMap });
+      Location.locations.forEach(loc => { if (!loc.onMap) loc.onMap = !loc.onMap });
+      MadamNazar.onMap = true;
+      Pins.onMap = true;
+    });
+
     $('.camps-small-btn').on('click', function () {
       $(this).toggleClass('disabled');
       Camp.isSmall = !Camp.isSmall;
-      Camp.locations.forEach(function(camp) { if(camp.layer['_map'] != null) camp.reinitMarker() });
+      Camp.locations.forEach(camp => { if (camp.layer['_map'] != null) camp.reinitMarker() });
     });
     $('.camps-large-btn').on('click', function () {
       $(this).toggleClass('disabled');
       Camp.isLarge = !Camp.isLarge;
-      Camp.locations.forEach(function(camp) { if(camp.layer['_map'] != null) camp.reinitMarker() });
+      Camp.locations.forEach(camp => { if (camp.layer['_map'] != null) camp.reinitMarker() });
     });
 
     $('.shops-hide-btn').on('click', function () {
-      Shop.locations.forEach(shop => { if(shop.onMap) shop.onMap = !shop.onMap});
+      Shop.locations.forEach(shop => { if (shop.onMap) shop.onMap = !shop.onMap });
     });
     $('.shops-show-btn').on('click', function () {
-      Shop.locations.forEach(shop => { if(!shop.onMap) shop.onMap = !shop.onMap});
+      Shop.locations.forEach(shop => { if (!shop.onMap) shop.onMap = !shop.onMap });
     });
 
     $('.gfh-hide-btn').on('click', function () {
-      GunForHire.locations.forEach(_gfh => { if(_gfh.onMap) _gfh.onMap = !_gfh.onMap});
+      GunForHire.locations.forEach(_gfh => { if (_gfh.onMap) _gfh.onMap = !_gfh.onMap });
     });
     $('.gfh-show-btn').on('click', function () {
-      GunForHire.locations.forEach(_gfh => { if(!_gfh.onMap) _gfh.onMap = !_gfh.onMap});
+      GunForHire.locations.forEach(_gfh => { if (!_gfh.onMap) _gfh.onMap = !_gfh.onMap });
     });
 
     $('.plants-hide-btn').on('click', function () {
-      PlantsCollection.locations.forEach(_plants => { if(_plants.onMap) _plants.onMap = !_plants.onMap});
+      PlantsCollection.locations.forEach(_plants => { if (_plants.onMap) _plants.onMap = !_plants.onMap });
     });
     $('.plants-show-btn').on('click', function () {
-      PlantsCollection.locations.forEach(_plants => { if(!_plants.onMap) _plants.onMap = !_plants.onMap});
+      PlantsCollection.locations.forEach(_plants => { if (!_plants.onMap) _plants.onMap = !_plants.onMap });
       setTimeout(() => PlantsCollection.layer.redraw(), 40);
     });
 
     $('.encounters-hide-btn').on('click', function () {
-      Encounter.locations.forEach(_encounter => { if(_encounter.onMap) _encounter.onMap = !_encounter.onMap});
+      Encounter.locations.forEach(_encounter => { if (_encounter.onMap) _encounter.onMap = !_encounter.onMap });
     });
     $('.encounters-show-btn').on('click', function () {
-      Encounter.locations.forEach(_encounter => { if(!_encounter.onMap) _encounter.onMap = !_encounter.onMap});
+      Encounter.locations.forEach(_encounter => { if (!_encounter.onMap) _encounter.onMap = !_encounter.onMap });
     });
   }
 }
