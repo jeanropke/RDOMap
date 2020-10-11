423cfc12f5eef5352c5d75fbc3201d0ec9542181
Michal__d <53737762+MichalD96@users.noreply.github.com>
2020-10-11 10:39:53 +0200

    Add missing language keys

--- assets/js/marker.js
+++ assets/js/marker.js
@@ -10,9 +10,10 @@ class Marker {
       switch (category) {
         case 'fasttravel':
           return Language.get(`${this.category}.${this.text}.name`);
+        case 'rescue':
+          return Language.get(`map.${this.category}.objective.name`);
         case 'shops':
         case 'gfh':
-        case 'rescue':
           return Language.get(`map.${this.category}.${this.subdata}.name`);
         case 'hideouts':
           return Language.get(`map.${this.category}.${this.text}.name`) + ' - ' + `[${convertToTime(this.subdata[0])} - ${convertToTime(this.subdata[1])}]`;
