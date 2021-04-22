9ae1738ade5c04d6e9227753b2927edc41fa6256
Senexis <timoviveen@gmail.com>
2021-04-22 23:07:28 +0200

    Only 1 commit for lang updates.
    TODO: Bail if no new translations, it's late now.

--- crowdin/update-lang-files.sh
+++ crowdin/update-lang-files.sh
@@ -4,10 +4,17 @@
 cd ..
 {
   git pull
+
+  # Lang files.
   crowdin download -b RDO
+  crowdin upload sources -b RDO
+  crowdin upload translations -b RDO
+
+  # Cont files.
+  node "crowdin/index.js"
+
+  # Commit.
   git add .
   git commit -m "Automatic language update."
   git push
-  crowdin upload sources -b RDO
-  crowdin upload translations -b RDO
-} >"logs/lang-file-$(date +"%Y-%m-%d_%H-%M").log" 2>&1
+} >"logs/lang-$(date +"%Y-%m-%d_%H-%M").log" 2>&1
